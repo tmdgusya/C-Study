245252e9dd4f5dbe77b0271788371ba0ef242549
roach <tmdgusya@naver.com>
2021-09-18 21:24:52 +0900

    init workspace

---
+++ C++Algorithm/array_stucy.cpp
@@ -0,0 +1,13 @@
+#include<array>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    array<int, 10> dodo;
+
+    dodo[0] = 1;
+
+    cout << "arr1 배열의 첫 번째 원소: " << dodo[0] << endl;
+}
---
+++ cstudy/cstudy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cstudy
+//
+//  Created by 정승현 on 2021/09/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
