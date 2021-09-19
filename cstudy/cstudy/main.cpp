70362178ab7fcbdf3c710a70f2fd857df20ed2ce
roach <tmdgusya@naver.com>
2021-09-19 14:14:44 +0900

    header file

--- cstudy/cstudy/main.cpp
+++ cstudy/cstudy/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream> //cout, cin, endl, ...
 #include <cstdio> // prinf
+#include "add.hpp"
 
 using namespace std;
 
 //function foward declartion
-int add(int a, int b);
 int multiply(int a, int b);
 
 int main(int argc, const char * argv[]) {
     cout << add(1, 2) << endl;
     return 0;
-}
-
-int add(int a, int b) {
-    return a + b;
 }
 
 int multiply(int a, int b) {
