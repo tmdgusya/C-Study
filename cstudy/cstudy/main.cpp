baba66e129735c4728d9358b69bcee88822fe382
roach <tmdgusya@naver.com>
2021-09-19 20:45:01 +0900

    void type

--- cstudy/cstudy/main.cpp
+++ cstudy/cstudy/main.cpp
@@ -5,22 +5,14 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     using namespace std;
-
-    bool bValue = false;
-    float fValue = 3.141592f;
     
-    auto aValue = 3.141592; // auto type
+    void *my_void;
     
-    cout << (bValue ? 1 : 0) << endl;
-    cout << fValue << endl;
-    cout << aValue << endl;
+    int i = 123;
     
-    int a(123); // direct initialization
-    int b{123}; // uniform initialization
+    my_void = (void*)&i;
     
-    cout << sizeof(aValue) << endl; // 8byte
-    cout << a << endl;
-    cout << b << endl;
+    cout << my_void << endl;
     
     return 0;
 }
