bd6a37e5e02aaa0cb13e09664f004b98f7c4614b
roach <tmdgusya@naver.com>
2021-09-20 10:41:26 +0900

    float

--- cstudy/cstudy/main.cpp
+++ cstudy/cstudy/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream> //cout, cin, endl, ...
-#include <bitset>
+#include <limits.h>
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     using namespace std;
     
-    void *my_void;
+    cout << numeric_limits<float>::lowest() << endl;
+    cout << numeric_limits<double>::lowest() << endl;
+    cout << numeric_limits<long double>::lowest() << endl;
     
-    int i = 123;
+    cout << 3.14 << endl;
+    cout << 31.4e-1 << endl;
     
-    my_void = (void*)&i;
-    
-    cout << my_void << endl;
     
     return 0;
 }
